841b3809831853abc767d8b2535d6027b7d987a8
hvardd <haavardsellingsen@gmail.com>
2022-05-17 02:27:24 +0200

    End of Section 5

---
+++ studentadminportal-ui/src/app/services/gender.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Gender } from '../models/api_models/gender.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenderService {
+
+  private baseApiUrl = 'https://localhost:44356';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  getGenderList(): Observable<Gender[]> {
+    return this.httpClient.get<Gender[]>(this.baseApiUrl + '/genders');
+  }
+}
